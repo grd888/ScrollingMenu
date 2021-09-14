7c86968e0c8d5f1c4b4bcd01201aa304911d10ad
Greg Delgado <grd888@gmail.com>
2021-09-14 10:58:25 +0800

    test viewController tableView outlet should be set

---
+++ ScrollingMenu/ScrollingMenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ScrollingMenuViewController.swift
+//  ScrollingMenu
+//
+//  Created by GD on 9/14/21.
+//
+
+import UIKit
+
+class ScrollingMenuViewController: UIViewController {
+    @IBOutlet private(set) public var tableView: UITableView!
+}
+
---
+++ ScrollingMenuTests/ScrollingMenuViewControllerTests.swift
@@ -0,0 +1,31 @@
+//
+//  ScrollingMenuViewControllerTests.swift
+//  ScrollingMenuTests
+//
+//  Created by GD on 9/14/21.
+//
+
+import XCTest
+import UIKit
+@testable import ScrollingMenu
+
+class ScrollingMenuViewControllerTests: XCTestCase {
+
+    func test_viewController_tableViewOutletShouldBeSet() {
+        let sut = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertNotNil(sut.tableView)
+    }
+    
+    // MARK: - Helper
+    
+    func makeSUT() -> ScrollingMenuViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(for: ScrollingMenuViewController.self))
+        let sut = storyboard.instantiateInitialViewController() as! ScrollingMenuViewController
+        
+        return sut
+    }
+
+}
