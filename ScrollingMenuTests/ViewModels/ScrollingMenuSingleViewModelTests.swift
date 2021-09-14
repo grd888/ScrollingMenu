73d06e3c14256d16feab10b310f73fb81a7114b2
Greg Delgado <grd888@gmail.com>
2021-09-14 16:40:59 +0800

    test `ScrollingMenuSingleViewModel` returns correct title and url

---
+++ ScrollingMenuTests/ViewModels/ScrollingMenuSingleViewModelTests.swift
@@ -0,0 +1,23 @@
+//
+//  ScrollingMenuSingleViewModelTests.swift
+//  ScrollingMenuTests
+//
+//  Created by GD on 9/14/21.
+//
+
+import XCTest
+@testable import ScrollingMenu
+
+class ScrollingMenuSingleViewModelTests: XCTestCase {
+    func test_sut_returnsCorrectComputedValues() {
+        let title = "a title"
+        let url = URL(string: "http://any-url.com")!
+        
+        let titleImageData = TitleImageDataMock(title: title, imageURL: url)
+        let sut = ScrollingMenuSingleViewModel(data: titleImageData)
+        
+        XCTAssertEqual(sut.title, title)
+        XCTAssertEqual(sut.url, url)
+    }
+
+}
